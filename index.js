b749612c209d7b9a4e457f34fc819cc8aaf18525
kymy44 <54514007+kymy44@users.noreply.github.com>
2024-06-03 08:26:55 +0100

    Funcional

---
+++ index.js
@@ -0,0 +1,22 @@
+//check palindromos
+
+let word = window.prompt("introduzca una palabra");
+
+//console.log(typeof word, word);
+
+word = word.split("");
+
+//console.log(typeof word, word);
+
+let flipped = [];
+console.log(flipped);
+
+for (let i = word.length; i > 0; i--) {
+	flipped.push(word[i - 1]);
+	console.log(flipped);
+}
+console.log(typeof word, word);
+console.log(typeof flipped, flipped);
+word.join === flipped.join //js no compara arrays directamente pq compara sus referencias a las variables
+	? console.log("Es palindromo")
+	: console.log("no es palindromo");
